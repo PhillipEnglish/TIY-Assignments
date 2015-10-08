11f05a0e9b0716efaa499bcf246aa3840af6c9aa
Phillip English <vitkiarts@gmail.com>
2015-10-08 16:01:27 -0400

    Added 2nd View and Segue

---
+++ OutOfTime/OutOfTime/DatePickerViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DatePickerViewController.swift
+//  OutOfTime
+//
+//  Created by Phillip English on 10/8/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
+{
+    
+    
+    
+ 
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillDisappear(animated: Bool)
+    {
+        super.viewWillDisappear(animated)
+      //  delegate?.timerWasChosen(60-picker.selectedRowInComponent(0))
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+ 
+    
+}
