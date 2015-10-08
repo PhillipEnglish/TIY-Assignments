069eb4bfdf56a8d42803dc1bcd7d3a8201690f61
Phillip English <vitkiarts@gmail.com>
2015-10-07 22:20:46 -0400

    Added code to check for text in both name and password files.

--- MissionBriefing/MissionBriefing/MissionBriefingViewController.swift
+++ MissionBriefing/MissionBriefing/MissionBriefingViewController.swift
@@ -42,15 +42,16 @@ class MissionBriefingViewController: UIViewController
     @IBAction func authenticateAgent(sender: UIButton)
     {
         // This will cause the keyboard to dismiss when the authenticate button is tapped
-        if authenticateAgent(<#T##sender: UIButton##UIButton#>).isFirstResponder
+        if agentName.isFirstResponder
         {
-            authenticateAgent(sender: UIButton).resignFirstResponder
+            agentName.resignFirstResponder
         }
         
         //
         // 4. Check whether there is text in BOTH the name and password textfields
         //
-        if <#?#>
+        if (agentName.text  != nil) && (agentPassWord.text != nil)
+
         {
             //
             // 5. The greeting label needs to be populated with the the string "Good evening, Agent #", where # is the last name of
@@ -59,7 +60,7 @@ class MissionBriefingViewController: UIViewController
             //    Strings". You should be able to find a method that allows you to break up a string using a delimiter. In our case,
             //    the delimiter would be a space character.
             //
-            
+           
             
             
             
