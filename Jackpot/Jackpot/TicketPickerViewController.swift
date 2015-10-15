a9010fe3b30c3a6d457c7d2e8b11b0774dd66122
Phillip English <vitkiarts@gmail.com>
2015-10-14 22:06:41 -0400

    Added TicketPicker view and controller class, added Segue to TicketPicker

---
+++ Jackpot/Jackpot/TicketPickerViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TicketPickerViewController.swift
+//  Jackpot
+//
+//  Created by Phillip English on 10/14/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import Cocoa
+
+class TicketPickerViewController: UIViewController {
+
+}
