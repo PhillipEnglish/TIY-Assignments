e66dd3608b776a4addb5a0b1b70060ed45773101
Phillip English <vitkiarts@gmail.com>
2015-10-27 15:24:53 -0400

    Changed view controller to tableview controller

--- GitHub-Friends/GitHub-Friends/FriendInfoTableViewController.swift
+++ GitHub-Friends/GitHub-Friends/FriendInfoTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FriendInfoTableViewController.swift
 //  GitHub-Friends
 //
 //  Created by Phillip English on 10/27/15.
@@ -8,17 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FriendInfoTableViewController: UITableViewController
 
-    override func viewDidLoad() {
+{
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
