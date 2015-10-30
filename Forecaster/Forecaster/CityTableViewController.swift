049b980dbd559a666b3dd0f30e2331edd3f88bb4
Phillip English <vitkiarts@gmail.com>
2015-10-30 14:54:45 -0400

    Initial Setup of Modal View Controller

--- Forecaster/Forecaster/CityTableViewController.swift
+++ Forecaster/Forecaster/CityTableViewController.swift
@@ -12,6 +12,8 @@ class CityTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationController?.navigationBar.barTintColor = UIColor.grayColor()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
---
+++ Forecaster/Forecaster/ModalZipCodeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ModalZipCodeViewController.swift
+//  Forecaster
+//
+//  Created by Phillip English on 10/30/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class ModalZipCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.barTintColor = UIColor.grayColor()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
