7d9770aab3e41e0a2d28557011010b9ae39f49c1
Phillip English <vitkiarts@gmail.com>
2015-11-03 18:33:37 -0500

    initial commit

---
+++ DudeWheresMyCar/Forecaster/CityWeatherCell.swift
@@ -0,0 +1,17 @@
+//
+//  CityWeatherCell.swift
+//  Forecaster
+//
+//  Created by Phillip English on 11/2/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import Cocoa
+
+class CityWeatherCell: UITableViewCell
+{
+
+    
+    
+    
+}
