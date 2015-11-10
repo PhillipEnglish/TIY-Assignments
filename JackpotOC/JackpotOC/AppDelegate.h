be544326edea3661c14d8916d05f9f4f2ee4bc8a
Phillip English <vitkiarts@gmail.com>
2015-11-10 12:37:21 -0500

    Initial Commit

---
+++ JackpotOC/JackpotOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JackpotOC
+//
+//  Created by Phillip English on 11/10/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
