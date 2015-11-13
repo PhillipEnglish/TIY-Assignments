136cfe2b6f7c033c44182e86b95fe4672c7ef549
Phillip English <vitkiarts@gmail.com>
2015-11-12 19:44:30 -0500

    Added UICollectionview. Added TitleViewController

---
+++ MovieMania/MovieMania/MoviesCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoviesCollectionViewController.h
+//  MovieMania
+//
+//  Created by Phillip English on 11/12/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoviesCollectionViewController : UICollectionViewController
+
+@end
