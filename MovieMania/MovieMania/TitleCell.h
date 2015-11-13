1a3873e2ceecf41b6982484de00a19212da10308
Phillip English <vitkiarts@gmail.com>
2015-11-13 10:44:56 -0500

    Generated initial file for custom UICollectionView cell

---
+++ MovieMania/MovieMania/TitleCell.h
@@ -0,0 +1,13 @@
+//
+//  MovieCell.h
+//  MovieMania
+//
+//  Created by Phillip English on 11/13/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TitleCell : UICollectionViewCell
+
+@end
---
+++ MovieMania/MovieMania/TitleCell.m
@@ -0,0 +1,13 @@
+//
+//  MovieCell.m
+//  MovieMania
+//
+//  Created by Phillip English on 11/13/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+#import "TitleCell.h"
+
+@implementation TitleCell
+
+@end
