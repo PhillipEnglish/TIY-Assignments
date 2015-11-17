059e9458dccc44758ce7f8ea66ea01d69c5913b4
Phillip English <vitkiarts@gmail.com>
2015-11-17 12:54:54 -0500

    Created ClockTableViewController class and ClockCell custom cell class files.

---
+++ GlobalTime/GlobalTime/ClockCell.swift
@@ -0,0 +1,24 @@
+//
+//  ClockCell.swift
+//  GlobalTime
+//
+//  Created by Phillip English on 11/17/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class ClockCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
