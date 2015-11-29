21423d9dcb0b887130a4a47dd15adc36fcc39303
Phillip English <vitkiarts@gmail.com>
2015-11-29 17:18:16 -0500

    Model and TableView added

---
+++ VenueMenu/VenueMenu/Venue+CoreDataProperties.swift
@@ -0,0 +1,18 @@
+//
+//  Venue+CoreDataProperties.swift
+//  VenueMenu
+//
+//  Created by Phillip English on 11/29/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Venue
+{
+    @NSManaged var name: String?
+    @NSManaged var address: String?
+    @NSManaged var lat: NSNumber?
+    @NSManaged var lng: NSNumber?
+}
